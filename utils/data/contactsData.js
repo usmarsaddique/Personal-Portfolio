712a1f6fdc0e0ed72a3a58a6191e46019f21154d
Umar91123 <umarfun12334@gmail.com>
2026-06-13 22:05:00 +0500

    location

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'umarsaddique582@gmail.com',
     phone: '+923068688327',
-    address: 'Sir Syed Town,Faisalabad - Pakistan ',
+    address: 'Lahore - Pakistan ',
     github: 'https://github.com/usmarsaddique',
     // facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/muhammad-umar-5b557b3ba/',
