f91389fe6d396e8e8e92dd1ebfa8eceb973409a2
Umar91123 <umarfun12334@gmail.com>
2026-06-14 01:25:30 +0500

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Muhammad Umar. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'umarsaddique582@gmail.com',
   phone: '+923068688327',
-  address: 'Sir Syed Town,Faisalabad - Pakistan ',
+  address: 'Lahore - Pakistan ',
   github: 'https://github.com/usmarsaddique',
   facebook: '',
   linkedIn: 'https://www.linkedin.com/in/muhammad-umar-5b557b3ba/',
