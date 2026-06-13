29643729e6b415818d35a036273d30ed13a103ee
Umar91123 <umarfun12334@gmail.com>
2026-06-13 06:31:20 +0500

    name changes

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+    email: 'umarsaddique582@gmail.com',
+    phone: '+923068688327',
+    address: 'Sir Syed Town,Faisalabad - Pakistan ',
     github: 'https://github.com/said7388',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
